c69bb12ca4e6bb1b630723664298852d0de3a73a
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2016-10-17 22:17:02 +0200

    add collision detection in lab 4a

--- 4. lokalne rozgłośnie/zadanie_a/calculatechannel.cpp
+++ 4. lokalne rozgłośnie/zadanie_a/calculatechannel.cpp
@@ -4,12 +4,27 @@
 
 
 //tu można pisać swoje implementacje (można dodpisać inne funkcje)
+bool stationsCollide(const StationConfiguration& stationConfiguration, int station1, int station2);
 
 bool checkColisions(int currentChannels[], int station, const StationConfiguration& stationConfiguration) {
-    return false;  // zmienić
+	// sprawdzić czy trzeba i <= station
+	for (int i = 0; i <= station; i++) {
+		for (int j = 0; j < i; j++) {
+			if (currentChannels[i] == currentChannels[j] && stationsCollide(stationConfiguration, i, j)) {
+				return true;
+			}
+		}
+	}
+    return false;
 }
 
 pair<int, int> calculateChannelCount(const StationConfiguration& stationConfiguration, int channels[]) {
     return make_pair(0, 0);  // zmienić
 }
 
+bool stationsCollide(const StationConfiguration& stationConfiguration, int station1, int station2) {
+	auto begin = stationConfiguration.interferringStations.begin();
+	auto end = stationConfiguration.interferringStations.end();
+	return (find(begin, end, make_pair(station1, station2)) != end ||
+		find(begin, end, make_pair(station2, station1)) != end);
+}
