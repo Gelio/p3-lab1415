8d69bf0869e83e66f42583e75acd7126df463fc5
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2016-10-28 18:03:38 +0200

    complete task 5a

--- 5. C# animals/zadanie_a/Animals.cs
+++ 5. C# animals/zadanie_a/Animals.cs
@@ -2,73 +2,135 @@
 
 namespace PO_Lab5
 {
-	// Napisać abstrakcyjną klasę Toy
-	// a w niej publiczne abstrakcyjne bezparametrowe metody Play i Eat
+    // Napisać abstrakcyjną klasę Toy
+    // a w niej publiczne abstrakcyjne bezparametrowe metody Play i Eat
 
-	abstract class Toy
-	{
-	abstract 
-	}
-	
-		abstract class Animal
-		{
-		public readonly string Name;
-		
-		public Toy(string nm):Toy(nm);
-		
-		virtual void Play()
-		{
-			Console.WriteLine("Animal ");
-			Console.WriteLine(Name);
-			Console.WriteLine(" is playing alone");
-		}
-		
-		abstract void Play(Toy t);
-		
-		abstract void Speak();
-				
-		}
-		
-		class Dog:Animal
-		{
-		
-		}
-		
-		class Cat:Animal
-		{
-		
-		}
-	
-	// Napisać abstrakcyjną klasę Animal
-	// z następującymi składowymi
-	// - publiczne pole Name typu string (pole readonly)
-	// - konstruktor z parametrem typu string (imię zwierzęcia)
-	// - wirtualna bezparametrowa metoda Play
-	//     wypisująca na konsolę "Animal {Name} is playing alone"
-	// - abstrakcyjna metoda Play z parametrem typu Toy
-	// - abstrakcyjna bezparametrowa metoda Speak
+    abstract class Toy
+    {
+        abstract public void Play();
+        abstract public void Eat();
+    }
 
-	// Napisać klasy Dog i Cat dzidziczące z klasy Animal
-	// obie klasy powinny zawierać konstruktor wskazujący imię zwierzęcia
+    // Napisać klasy Bone i FurryBall dziedziczące z Toy
+    // Metody klasy Bone działają w następujący sposób
+    //  Play wypisuje na konsolę "Playing with bone."
+    //  Eat wypisuje na konsolę "Yummy bone."
 
-	// Metody klasy Dog działają w następujący sposób:
-	//  Speak wypisuje na konsolę "{Name}: woof, woof"
-	//  bezparametrowa metoda Play nie jest nadpisana
-	//  metada Play przyjmująca jako parametr Toy - wypisuje na konsolę "{Name} is eating toy: " i wywołuje metodę Eat na parametrze.
+    class Bone : Toy
+    {
+        public override void Play()
+        {
+            Console.WriteLine("Playing with a bone.");
+        }
 
-	// Metody klasy Cat działają w następujący sposób:
-	//  Speak wypisuje na konsolę "{Name}: mew, mew"
-	//  bezparametrowa metoda Play wypisuję na konsolę "{Name}: I want a toy !!!"
-	//  metada play przyjmująca jako parametr Toy - wypisuje na konsolę "{Name} is playing with a toy: " i wywołuje metodę Play na parametrze.
+        public override void Eat()
+        {
+            Console.WriteLine("Yummy bone.");
+        }
+    }
 
-	// Napisać klasy Bone i FurryBall dziedziczące z Toy
-	// Metody klasy Bone działają w następujący sposób
-	//  Play wypisuje na konsolę "Playing with bone."
-	//  Eat wypisuje na konsolę "Yummy bone."
+    // Metody klasy FurryBall działają w następujący sposób
+    //  Play wypisuje na konsolę "Playing with furry ball."
+    //  Eat wypisuje na konsolę "Bleh this is un-eatable."
 
-	// Metody klasy FurryBall działają w następujący sposób
-	//  Play wypisuje na konsolę "Playing with furry ball."
-	//  Eat wypisuje na konsolę "Bleh this is un-eatable."
+    class FurryBall : Toy
+    {
+        public override void Play()
+        {
+            Console.WriteLine("Playing with furry ball.");
+        }
 
+        public override void Eat()
+        {
+            Console.WriteLine("Bleh this is un-eatable.");
+        }
+    }
+
+    // Napisać abstrakcyjną klasę Animal
+    // z następującymi składowymi
+    // - publiczne pole Name typu string (pole readonly)
+    // - konstruktor z parametrem typu string (imię zwierzęcia)
+    // - wirtualna bezparametrowa metoda Play
+    //     wypisująca na konsolę "Animal {Name} is playing alone"
+    // - abstrakcyjna metoda Play z parametrem typu Toy
+    // - abstrakcyjna bezparametrowa metoda Speak
+
+
+    abstract class Animal
+    {
+        public readonly string Name;
+
+        public Animal(string name)
+        {
+            this.Name = name;
+        }
+
+        virtual public void Play()
+        {
+            Console.WriteLine("Animal {0} is playing alone", this.Name);
+        }
+
+        abstract public void Play(Toy t);
+
+        abstract public void Speak();
+
+    }
+
+    // Napisać klasy Dog i Cat dzidziczące z klasy Animal
+    // obie klasy powinny zawierać konstruktor wskazujący imię zwierzęcia
+
+    // Metody klasy Dog działają w następujący sposób:
+    //  Speak wypisuje na konsolę "{Name}: woof, woof"
+    //  bezparametrowa metoda Play nie jest nadpisana
+    //  metada Play przyjmująca jako parametr Toy - wypisuje na konsolę "{Name} is eating toy: " i wywołuje metodę Eat na parametrze.
+
+    class Dog : Animal
+    {
+        public Dog(string name):base(name)
+        {
+            
+        }
+
+        public override void Play(Toy t)
+        {
+            Console.WriteLine("{0} is eating toy: ", this.Name);
+            t.Eat();
+        }
+
+        public override void Speak()
+        {
+            Console.WriteLine("{0}: woof, woof", this.Name);
+        }
+    }
+
+
+    // Metody klasy Cat działają w następujący sposób:
+    //  Speak wypisuje na konsolę "{Name}: mew, mew"
+    //  bezparametrowa metoda Play wypisuję na konsolę "{Name}: I want a toy !!!"
+    //  metada play przyjmująca jako parametr Toy - wypisuje na konsolę "{Name} is playing with a toy: " i wywołuje metodę Play na parametrze.
+
+    class Cat : Animal
+    {
+        public Cat(string name):base(name)
+        {
+
+        }
+
+        public override void Play()
+        {
+            Console.WriteLine("{0}: I want a toy !!!", this.Name);
+        }
+
+        public override void Play(Toy t)
+        {
+            Console.WriteLine("{0} is playing with a toy: ", this.Name);
+            t.Eat();
+        }
+
+        public override void Speak()
+        {
+            Console.WriteLine("{0}: mew, mew", this.Name);
+        }
+    }
 
 }
--- 5. C# animals/zadanie_a/Program.cs
+++ 5. C# animals/zadanie_a/Program.cs
@@ -6,34 +6,34 @@ namespace PO_Lab5
 	{
 		static void Main()
 		{
-            //Animal[] animals = { new Dog("Odie"), new Cat("Garfield"), new Dog("Dogbert"), };
+            Animal[] animals = { new Dog("Odie"), new Cat("Garfield"), new Dog("Dogbert"), };
 
             //// Etap 1 (2p.)
-            //Console.WriteLine("\nAnimals speak:");
-            //for (int i = 0; i < animals.Length; i++)
-            //{
-            //    animals[i].Speak();
-            //}
+            Console.WriteLine("\nAnimals speak:");
+            for (int i = 0; i < animals.Length; i++)
+            {
+                animals[i].Speak();
+            }
 
             //// Etap 2 (1p.)
-            //Console.WriteLine("\nAnimals play:");
-            //for (int i = 0; i < animals.Length; i++)
-            //{
-            //    animals[i].Play();
-            //}
+            Console.WriteLine("\nAnimals play:");
+            for (int i = 0; i < animals.Length; i++)
+            {
+                animals[i].Play();
+            }
 
             //// Etap 3 (2p.)
-            //Toy[] toys = {new Bone(), new FurryBall()};
+            Toy[] toys = { new Bone(), new FurryBall() };
 
-            //Console.WriteLine("\nAnimals play with toys:");
-            //for (int i = 0; i < animals.Length; i++)
-            //{
-            //    Console.WriteLine();
-            //    for (int j = 0; j < toys.Length; j++)
-            //    {
-            //        animals[i].Play(toys[j]);
-            //    }
-            //}
+            Console.WriteLine("\nAnimals play with toys:");
+            for (int i = 0; i < animals.Length; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < toys.Length; j++)
+                {
+                    animals[i].Play(toys[j]);
+                }
+            }
 
             Console.WriteLine();
 		}
