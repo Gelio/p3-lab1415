b46ac12e3876413ff24327089046ed578e069094
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2016-12-07 18:52:50 +0100

    lab10: fix Diff1 and Diff2, complete stage 5

--- 10. wielomiany delegaty/zadanie_a/Program.cs
+++ 10. wielomiany delegaty/zadanie_a/Program.cs
@@ -55,14 +55,14 @@ namespace lab10
             return x0;
         }
 
-        public static Function Diff1(Pol p)
+        public static Function Diff1(Function f)
         {
-            return x => (p.Horner(x + eps) - p.Horner(x - eps)) / (2 * eps);
+            return x => (f(x + eps) - f(x - eps)) / (2 * eps);
         }
 
-        public static Function Diff2(Pol p)
+        public static Function Diff2(Function f)
         {
-            return x => (p.Horner(x + eps) - 2 * p.Horner(x) + p.Horner(x - eps)) / (eps * eps);
+            return x => (f(x + eps) - 2 * f(x) + f(x - eps)) / (eps * eps);
         }
 
         public static Function Suma(params Function[] fArr)
@@ -113,9 +113,9 @@ namespace lab10
             double x = 2;
             Console.WriteLine("  P(2) = " + P.Horner(2));
             Console.WriteLine(" P1(2) = " + P.Diff1().Horner(2));
-            Console.WriteLine("~P1(2) = " + Diff1(P)(2));
+            Console.WriteLine("~P1(2) = " + Diff1(P.Horner)(2));
             Console.WriteLine(" P2(2) = " + P.Diff2().Horner(2));
-            Console.WriteLine("~P2(2) = " + Diff2(P)(2));
+            Console.WriteLine("~P2(2) = " + Diff2(P.Horner)(2));
 
             
 
@@ -131,8 +131,8 @@ namespace lab10
             p1 = P.Diff1().Horner;
             m_Bisection = Bisection(p1, a, b);
 
-            p1 = Diff1(P);
-            p2 = Diff2(P);
+            p1 = Diff1(P.Horner);
+            p2 = Diff2(P.Horner);
             m_Newton = Newton(p1, p2, a);
 
             // wypisac znalezione minima wielomianu P wykorzystuj¹c metody bisekcji i Newtona
@@ -140,11 +140,11 @@ namespace lab10
 
             Console.WriteLine("m_Bisection = " + m_Bisection);
             Console.WriteLine(" P1(m_Bisection) = " + P.Diff1().Horner(m_Bisection));
-            Console.WriteLine("~P1(m_Bisection) = " + Diff1(P)(m_Bisection));
+            Console.WriteLine("~P1(m_Bisection) = " + Diff1(P.Horner)(m_Bisection));
 
             Console.WriteLine("   m_Newton = " + m_Newton);
             Console.WriteLine(" P1(m_Newton) = " + P.Diff1().Horner(m_Newton));
-            Console.WriteLine("~P1(m_Newton) = " + Diff1(P)(m_Newton));
+            Console.WriteLine("~P1(m_Newton) = " + Diff1(P.Horner)(m_Newton));
 
             
 
@@ -167,13 +167,27 @@ namespace lab10
 
             Console.WriteLine("suma operator bisekcja: " + Bisection(f, a, b));
 
-            f = Suma(Diff1(wiel1), Diff1(wiel2), Diff1(wiel3));
+            f = Suma(Diff1(wiel1.Horner), Diff1(wiel2.Horner), Diff1(wiel3.Horner));
             Console.WriteLine("suma funkcja bisekcja: " + Bisection(f, a, b));
 
             Console.WriteLine();
             Console.WriteLine("----------------   ETAP 5   ------------------------");
             //funkcje anonimowe oraz wyr. lambda
 
+            a = 0;
+            b = 4;
+
+            f = delegate (double t) { return 2 - 4 * t + Math.Exp(t); };
+            Console.WriteLine("metoda anonimowa bisekcja: " + Bisection(Diff1(f), a, b));
+            Console.WriteLine("  metoda anonimowa newton: " + Newton(Diff1(f), Diff2(f), a));
+
+            f = t => (2 - 4 * t + Math.Exp(t));
+            Console.WriteLine("wyrazenie lambda bisekcja: " + Bisection(Diff1(f), a, b));
+            Console.WriteLine("  wyrazenie lambda newton: " + Newton(Diff1(f), Diff2(f), a));
+
+            f = Suma(t => (2 - 4*t), t => Math.Exp(t));
+            Console.WriteLine("wyrazenie lambda bisekcja: " + Bisection(Diff1(f), a, b));
+            Console.WriteLine("  wyrazenie lambda newton: " + Newton(Diff1(f), Diff2(f), a));
         }
    
     }
